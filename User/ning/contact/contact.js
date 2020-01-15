972e266e29194890a929ec5b470e22df95f7370f
Nekophobia <jawbajaw40@gmail.com>
2020-01-15 22:41:06 +0800

    register page

--- User/ning/contact/contact.js
+++ User/ning/contact/contact.js
@@ -1,9 +1,29 @@
 var old;
 
-$("#phone, #cs, #email").hover(
+$("#phone").hover(
   function() {
     old = $(this).text();
     $(this).text("(855)326-9945");
+  },
+  function() {
+    $(this).text(old);
+  }
+);
+
+$("#cs").hover(
+  function() {
+    old = $(this).text();
+    $(this).text("getticket.com/support");
+  },
+  function() {
+    $(this).text(old);
+  }
+);
+
+$("#email").hover(
+  function() {
+    old = $(this).text();
+    $(this).text("getticket@gmail.com");
   },
   function() {
     $(this).text(old);
